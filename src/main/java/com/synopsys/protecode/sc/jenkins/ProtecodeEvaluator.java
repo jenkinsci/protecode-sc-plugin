04d87b79d2107e121feb1027f11e70e2991d4772
Lauri Pajunen <pajunen@synopsys.com>
2018-01-31 08:46:47 +0200

    Changed verdict evaluation logic: true means success now

--- src/main/java/com/synopsys/protecode/sc/jenkins/ProtecodeEvaluator.java
+++ src/main/java/com/synopsys/protecode/sc/jenkins/ProtecodeEvaluator.java
@@ -12,16 +12,23 @@ package com.synopsys.protecode.sc.jenkins;
 
 import com.synopsys.protecode.sc.jenkins.types.InternalTypes;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class ProtecodeEvaluator {
+  private static final Logger LOGGER = Logger.getLogger(ProtecodeEvaluator.class.getName());
+  
   public static boolean evaluate(
     List<InternalTypes.FileAndResult> results
   ) {
+    LOGGER.log(Level.ALL, "Evaluating");
     return results.stream().anyMatch((fileAndResult) -> {
       if (!fileAndResult.hasError()) {
-        return !fileAndResult.verdict();
+        LOGGER.log(Level.ALL, fileAndResult.getFilename() + "has result: " + fileAndResult.verdict());
+        return fileAndResult.verdict();
       } else {
+        LOGGER.log(Level.ALL, fileAndResult.getFilename() + "has error: " + fileAndResult.getError());
         return false;
       }
     });
