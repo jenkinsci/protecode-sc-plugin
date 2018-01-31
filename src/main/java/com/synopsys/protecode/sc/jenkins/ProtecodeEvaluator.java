9f1ff8b39dd415fcb4aff36812a55b94312d0145
Lauri Pajunen <pajunen@synopsys.com>
2018-01-31 11:50:09 +0200

    Correct logic mistake in build evaluation

--- src/main/java/com/synopsys/protecode/sc/jenkins/ProtecodeEvaluator.java
+++ src/main/java/com/synopsys/protecode/sc/jenkins/ProtecodeEvaluator.java
@@ -19,14 +19,19 @@ import java.util.logging.Logger;
 public class ProtecodeEvaluator {
   private static final Logger LOGGER = Logger.getLogger(ProtecodeEvaluator.class.getName());
   
+  /**
+   * 
+   * @param results All the results for the given scan.
+   * @return false if any vuln was found. Otherwise true
+   */
   public static boolean evaluate(
     List<InternalTypes.FileAndResult> results
   ) {
     LOGGER.log(Level.ALL, "Evaluating");
-    return results.stream().anyMatch((fileAndResult) -> {
+    return !results.stream().anyMatch((fileAndResult) -> {
       if (!fileAndResult.hasError()) {
         LOGGER.log(Level.ALL, fileAndResult.getFilename() + "has result: " + fileAndResult.verdict());
-        return fileAndResult.verdict();
+        return !fileAndResult.verdict();
       } else {
         LOGGER.log(Level.ALL, fileAndResult.getFilename() + "has error: " + fileAndResult.getError());
         return false;
