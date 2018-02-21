71b71dd042a7e73767af6a1b6b1f61524fabbcea
rukkanen <rukkanen@users.noreply.github.com>
2018-02-21 14:59:14 +0200

    First implementation of serializable pipewriter for file transfer

--- src/main/java/com/synopsys/protecode/sc/jenkins/types/StreamRequestBody.java
+++ src/main/java/com/synopsys/protecode/sc/jenkins/types/StreamRequestBody.java
@@ -10,11 +10,14 @@
   *******************************************************************************/
 package com.synopsys.protecode.sc.jenkins.types;
 
+import hudson.remoting.VirtualChannel;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.annotation.Nullable;
+import jenkins.MasterToSlaveFileCallable;
 import lombok.NonNull;
 import okhttp3.MediaType;
 import okhttp3.RequestBody;
@@ -57,26 +60,47 @@ public class StreamRequestBody extends RequestBody {
   
   @Override
   public void writeTo(@NonNull BufferedSink sink) {
-    InputStream inputStream = null;
-    Source source = null;
     try {
-      inputStream = file.read();
-      long writeAmount = inputStream.available();
-      while (writeAmount != 0) {
-        source = Okio.source(inputStream);
-        sink.write(source, writeAmount);
-        sink.flush();
-        writeAmount = inputStream.available();
-      }
-    } catch (Exception e) {
-      LOGGER.log(Level.WARNING, "Error while sending file. Error message: {0}", e.getMessage());
-    } finally {      
+    file.getFilePath().act(new PipeWriterCallable(sink, file));
+    } catch (IOException | InterruptedException ioe) {
+      LOGGER.log(Level.WARNING, "Serializing pipewriter error: {0}", ioe.getMessage());
+    }
+  }
+
+  // Slave support
+  private static class PipeWriterCallable extends MasterToSlaveFileCallable<Void> {
+    private static final long serialVersionUID = 2;
+    private final BufferedSink sink;
+    private final ReadableFile file;
+
+    public PipeWriterCallable(BufferedSink sink, ReadableFile file) {
+      this.sink = sink;
+      this.file = file;
+    }
+
+    @Override public Void invoke(File f, VirtualChannel channel) {
+      InputStream inputStream = null;
+      Source source = null;
       try {
-        inputStream.close();
+        inputStream = file.read();
+        long writeAmount = inputStream.available();
+        while (writeAmount != 0) {
+          source = Okio.source(inputStream);
+          sink.write(source, writeAmount);
+          sink.flush();
+          writeAmount = inputStream.available();
+        }
       } catch (Exception e) {
-        // No action: stream might have not been opened.    
+        LOGGER.log(Level.WARNING, "Error while sending file. Error message: {0}", e.getMessage());
+      } finally {
+        try {
+          inputStream.close();
+        } catch (Exception e) {
+          // No action: stream might have not been opened.
+        }
+        Util.closeQuietly(source);
       }
-      Util.closeQuietly(source);
+      return null;
     }
   }
 }
